256e7ac94892ddf7f98fdaa45102ac8963ac7d04
ragingfire35 <ragingfire35@163.com>
2017-07-07 21:34:51 +0800

    new branch for ios puch

--- samples/LFLiveKitFrameworkDemo/LFLiveKitFrameworkDemo/LFLivePreview.m
+++ samples/LFLiveKitFrameworkDemo/LFLiveKitFrameworkDemo/LFLivePreview.m
@@ -359,7 +359,8 @@ inline static NSString *formatedSpeed(float bytes, float elapsed_milli) {
             if (_self.startLiveButton.selected) {
                 [_self.startLiveButton setTitle:@"结束直播" forState:UIControlStateNormal];
                 LFLiveStreamInfo *stream = [LFLiveStreamInfo new];
-                stream.url = @"rtmp://live.hkstv.hk.lxdns.com:1935/live/stream153";
+                //stream.url = @"rtmp://live.hkstv.hk.lxdns.com:1935/live/stream153";
+                stream.url = @"rtmp://wansu-global-push-rtmp.17app.co/vod/dhlutest?wsSecret=d240a17cdc4aab4a2c9f23099ffaa0f9&wsTime=1499434028&id=dhlutest";
                 [_self.session startLive:stream];
             } else {
                 [_self.startLiveButton setTitle:@"开始直播" forState:UIControlStateNormal];
