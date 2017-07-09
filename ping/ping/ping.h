f19688d6bd8e8d56c3498d1bde4ebd0095130f07
ragingfire35 <ragingfire35@163.com>
2017-07-09 20:22:22 +0800

    add ping file

---
+++ ping/ping/ping.h
@@ -0,0 +1,19 @@
+//
+//  ping.h
+//  ping
+//
+//  Created by mymac on 2017/6/8.
+//  Copyright © 2017年 mymac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ping.
+FOUNDATION_EXPORT double pingVersionNumber;
+
+//! Project version string for ping.
+FOUNDATION_EXPORT const unsigned char pingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ping/PublicHeader.h>
+
+
